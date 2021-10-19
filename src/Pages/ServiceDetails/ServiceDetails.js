5ed3ee04c8074e4ec9bc36560215b8bcc13bf114
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-10-19 22:49:28 +0530

    extra on service detaisl route

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -21,7 +21,7 @@ const ServiceDetails = () => {
     }, [services])
 
     // const { name, img, shortDescription } = service;
-    // console.log('from service details-', service);
+    console.log('from service details-', service);
     return (
         <div className="custom-min-height">
             <Container className="bg-light my-5">
@@ -32,8 +32,10 @@ const ServiceDetails = () => {
                     </Col>
                     <Col>
                         <h1>{service?.name}</h1>
-                        <p>{service?.shortDescription}</p>
-                        <Badge className="d-block w-25 mx-auto my-3" pill bg="info">Price:$250</Badge>
+                        <p className="fs-5">{service?.shortDescription}</p>
+                        <h2>What is {service?.name}?</h2>
+                        <p className="fs-5">{service?.extraDescription}</p>
+                        <Badge className="d-block w-25 mx-auto my-3" pill bg="info">Price:${service?.price}</Badge>
                         <HashLink to="/home#services">
                             <Button variant="warning" >Go Back</Button>
                         </HashLink >
