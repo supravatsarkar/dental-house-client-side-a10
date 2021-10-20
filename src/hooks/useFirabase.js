fdc6f6a8a9db75217ecc88631004f1958833337b
Supravat Sarkar <supravatsarkar18@gmail.com>
2021-10-20 11:52:48 +0530

    [fixed] private route not redict to login page due to isloading true false

--- src/hooks/useFirabase.js
+++ src/hooks/useFirabase.js
@@ -81,6 +81,7 @@ const useFirebase = () => {
                 setLoading(false);
             } else {
                 setUser({});
+                setLoading(false);
             }
         })
     }, [])
